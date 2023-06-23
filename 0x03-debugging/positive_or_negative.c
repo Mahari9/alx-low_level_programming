049a0222200d37867ea051c5887a5ba45da879ca
Mahari9 <merytseg77@gmail.com>
2023-06-23 05:51:42 -0700

    3-print_remaining_days.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
 /**
- * main - Prints if number is positive, zero or negative
- *
- * Return: Always (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
 
-	return(0);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
