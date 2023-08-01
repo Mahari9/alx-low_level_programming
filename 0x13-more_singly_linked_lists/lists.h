78f22fd06b9aee0f525288d94ff547e5c66fcefc
Mahari9 <merytseg77@gmail.com>
2023-08-01 14:12:35 -0700

    betty fixed lists.h file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,11 +5,12 @@
 #include <stdio.h>
 
 /**
- * struct list_s - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for algorithm data structure project
  */
 typedef struct listint_s
 {
