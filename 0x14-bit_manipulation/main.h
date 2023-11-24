338719a664538fb105fbd89cf3b46b028639157b
Mahari9 <merytseg77@gmail.com>
2023-11-23 23:30:04 -0800

    fixing header

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
 int _putchar(char c);
 int get_endianness(void);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
@@ -14,4 +11,3 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 
 #endif
-
